c2c269bbcf549dc4f5702632bf3bb686180dd548
kratikasaxena1021 <kratikasaxena0613@gmail.com>
2018-07-19 18:43:32 +0530

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { UserListComponent } from './users/user-list/user-list.component';
+import { UserDetailComponent } from './users/user-detail/user-detail.component';
+import { UserNewComponent } from './users/user-new/user-new.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserListComponent,
+    UserDetailComponent,
+    UserNewComponent
   ],
   imports: [
     BrowserModule
